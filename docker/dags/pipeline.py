bce8da1155033f82fb898cad13a70d94870dd931
mariokojima <mariokojima@gmail.com>
2025-04-12 11:35:11 -0300

    add dag file

---
+++ docker/dags/pipeline.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operator.python_operator import PythonOperator
+from datetime import datetime
+import pandas as pd
+
+
+def generate_csv():
+    data={
+        'nomes': ['mario','erika','eric','nina']
+    }
+    df = pd.DataFrame(data)
+    df.to_csv('teste_dag.csv')
+
+
+default_args = {
+    'owner':'Mário Kojima',
+    'start_date': datetime(2025,4,12)
+}
+
+dag =  DAG(
+    'dag_gera_csv',
+    default_args = default_args,
+    description = 'Teste Airflow',
+    schedule_interval = None,
+    catchup=False,
+    tags=['teste']
+)
+
+tsk_gera_csv = PythonOperator(
+    task_id = 'gera_csv',
+    python_callable = generate_csv,
+    dag=dag,
+)
+
+tsk_gera_csv
